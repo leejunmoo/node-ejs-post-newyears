01179e305eb3b2bab7ceda652e1e690fa719e2d7
leejunmoo <dlquddnr122@naver.com>
2022-12-23 01:24:39 +0900

    npm moment 설치 -데이터 객체

--- index.js
+++ index.js
@@ -2,19 +2,26 @@ const express = require('express');
 const app = express();
 const ejs = require('ejs');
 const fs = require('fs');
+const moment = require('moment'); // 날짜구하는 npm 모듈
+const today = moment().format("YYYY-MM-DD");
 
 
 // 글 DB
 let posts = [];
 let postsName = [];
+let postsDate = [];
 const readfile = fs.readFileSync('postDB.json', 'utf-8');
 const redafileName = fs.readFileSync('postnameDB.json', 'utf-8');
+const redafileDate = fs.readFileSync('postdateDB.json', 'utf-8');
+
 // 오브젝트 코드로 변환
 const jsonData = JSON.parse(readfile);
 const jsonnameData = JSON.parse(redafileName);
+const jsondateData = JSON.parse(redafileDate);
 // console.log(jsonData);
 posts = [...jsonData]; // post에 배열값 추가
 postsName = [...jsonnameData];
+postsDate = [...jsondateData];
 
 // post 요청시 필요
 app.use(express.json())
@@ -28,23 +35,37 @@ app.use(express.static("public"));
 
 // home
 app.get('/', function(요청, 응답){
-  응답.render('pages/index.ejs', { posts, postsName })
+  응답.render('pages/index.ejs', { posts, postsName, postsDate, today })
 })
 
 // create
 app.post('/create', function(req, res) {
+ 
   const 글 = req.body.post;
   const name = req.body.postName;
+  const date = req.body.date;
+  postsDate.push(date);
   postsName.push(name);
   posts.push(글);
 
   // posts 배열에 글추가
-  console.log(posts);
+  console.log(postsDate);
   // DB file 에 글 저장
   fs.writeFileSync('postDB.json', JSON.stringify(posts))
   fs.writeFileSync('postnameDB.json', JSON.stringify(postsName))
+  fs.writeFileSync('postdateDB.json', JSON.stringify(postsDate))
   // 홈(게시판)으로 이동
   res.redirect('/');
+})
+
+// 글 삭제 요청
+app.post('/delete/:id', function(req, res){
+  const id = req.params.id;
+  //  누르는 버튼에 해당하는 id 삭제
+  posts.splice(id, 1)
+  fs.writeFileSync('postDB.json', JSON.stringify(posts))
+  res.redirect('/');
+
 })
 
 
